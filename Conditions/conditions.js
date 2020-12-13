cf0a09c9d819b137066d7203f5ca9cbe5e611ff6
28dimon99 <gonchardv101189@gmail.com>
2020-12-13 21:14:26 +0200

    consoleProject

---
+++ Conditions/conditions.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/*if (4 == 5){
+    console.log("Ok!")
+}
+else {
+    console.log("Error!")
+}*/
+
+/*const num = 50;
+if(num < 49){
+    console.log("Error!")
+}else if(num > 100){
+    console.log("Много!")
+}else{
+    console.log("Ok!")
+}*/
+
+/*
+const num = 66;
+(num == 50) ? console.log('Ok') : console.log('Error');
+*/
+
+const num = "hh";
+switch (num){
+    case 77:
+        console.log('Error');
+        break;
+    case 100:
+        console.log('Error');
+        break;
+    case 66:
+        console.log('Ok!');
+        break;
+    default :
+        console.log('NaN!')
+        break;
+}
+
---
+++ Cycles/cycles.js
@@ -0,0 +1,25 @@
+'use strict';
+let num = 100;
+/*while (num <= 105){
+    console.log(num);
+    num++;
+}*/
+/*do{
+    console.log(num);
+    num++
+}
+
+while(num<105);*/
+
+/*for(let i = 0; i<15+1; i++){
+    console.log(num);
+    num ++
+}*/
+
+for(let i = 0; i<10; i++){
+    if(i===7){
+     //break;
+        continue;
+    }
+    console.log(i)
+}
